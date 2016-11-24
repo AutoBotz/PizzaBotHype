3372d3fd7f84be0c60003442a6462835b4aab502
junka96 <andrej.janda@yahoo.ca>
2016-11-23 20:05:02 -0500

    switched cosine and sine

--- src/PizzaBotPkg/pizza_bot.java
+++ src/PizzaBotPkg/pizza_bot.java
@@ -20,45 +20,57 @@ public class pizza_bot {
 
 	public static drive_control robot = new drive_control();
 	public static user_interface UI = new user_interface();
-	
+
 	public static void main(String[] args) {
 		robot.gyro_init(1);
 		robot.sonic_init(2);
 		robot.init_pos(0.0, 0.0);
 		robot.set_dims(4, 4, 12);
-	
+
 	    move_to_point(15,15, robot);
 	    move_to_point(15,-15, robot);
 	    move_to_point(0, 0, robot);
-	    
+
 	    robot.spotTurn_gyro(0);
 	    robot.flt();
+<<<<<<< Updated upstream
 	    
 	    while(true){
 	    	
+=======
+
+	    while(false){
+
+>>>>>>> Stashed changes
 	    	// Exit code
 	    	if (Button.ESCAPE.isDown()) {
 	    	Motor.A.flt();
 	    	Motor.B.flt();
 	    	Delay.msDelay(1000);
-    
+
 	    	if (Button.ESCAPE.isDown()){break;}
 	    	}
 	    	// End Exit code
+<<<<<<< Updated upstream
 	    	
 	    	while(robot.ping() > 15){
 	    		robot.forward(5, 5);
+=======
+
+	    	while(true){
+
+>>>>>>> Stashed changes
 	    	}
-	    	
+
 	    }
 	}
-	
+
 	public static void move_to_point(int x, int y, drive_control robot) {
 		/**
 		 * This function accept the physical dimensions of the robot, and computes the corrections factors
 		 * for functions such as rotation, turn and forward driving to allow user to input reasonable numbers
 		 * into the control functions. Such as centimeters and centimeters per second.
-		 * 
+		 *
 		 * Returns nothing
 		 *
 		 * @param left_diameter Diameter of the left wheel
@@ -66,31 +78,31 @@ public class pizza_bot {
 		 * @param wheel_base The inner distance between two wheels
 		 * @param wheel_width The width of the wheels
 		 */
-		
+
 		UI.println("Current Pos (" + (int)robot.X + " , " + (int)robot.Y +" , " +(int)robot.theta() +")");
 
-		double delta_angle = (180*Math.atan((y-robot.Y)/(x-robot.X))/3.14159);
+		double delta_angle = (180*Math.atan((y-robot.Y)/(x-robot.X))/pi);
 		double distance = Math.sqrt((y-robot.Y)*(y-robot.Y) + (x-robot.X)*(x-robot.X));
-		
+
 
 		// driving reverse
 		if ((y-robot.Y)<0){
 			if ((x-robot.X)<0){
-				delta_angle = -90-delta_angle;
+				delta_angle = -90 - delta_angle;
 			}
 			else
-				delta_angle = 90 - delta_angle;
+				delta_angle =  90 - delta_angle;
 		}
 
 		
 		robot.spotTurn_gyro((int)delta_angle);
-		
+
 		System.out.println("going foreward");
-		
+
 		robot.forward((int)distance, 100);
-		
+
 	}
-	
+
 	public static int obstacle_encounter(float[] distance_array){
 		/**
 		 * This function accepts a array of 9 distance measurements and return the
@@ -98,12 +110,12 @@ public class pizza_bot {
 		 *
 		 * @param distance_array array of 9 distance measurements
 		 */
-		
+
 		// Distance of robot to object distance_array[4]
-		
+
 		float curr_max = distance_array[4];
 		int max_direction = 4;
-		
+
 		for(int i = 0; i < 9; i++){
 			if (distance_array[i] < 250 && distance_array[i] > curr_max){
 				curr_max = distance_array[i];
@@ -112,7 +124,4 @@ public class pizza_bot {
 		}
 		return (int)(max_direction*22.5 - 90);
 	}
-
-
 }
-
